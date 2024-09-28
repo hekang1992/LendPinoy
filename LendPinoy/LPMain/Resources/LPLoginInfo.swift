13353f1b31245e1b22380c20cc78887e8efe9259
何康 <hekang19921111@hotmail.com>
2024-09-28 16:38:09 +0800

    ref:优化

--- LendPinoy/LPMain/Resources/LPLoginInfo.swift
+++ LendPinoy/LPMain/Resources/LPLoginInfo.swift
@@ -35,29 +35,28 @@ extension LPLoginInfo {
     }
     
     static func getLogiInfo() -> [String: String] {
-        let sessionId = UserDefaults.standard.string(forKey: LP_SESSIONID) ?? ""
-        var logInfo: [String: String] = [
-            "appVersion": getAppVersion(),
-            "systemVersion": UIDevice.current.systemVersion,
-            "colorTheme": "pink",
-            "contentType": "testjson",
-            "sessionId": sessionId,
-            "carModel1": "m3",
-            "carModel2": "s6",
-            "lighterBrand": "fire"
-        ]
+        let moist = UserDefaults.standard.string(forKey: LP_SESSIONID) ?? ""
         
-        let additionalInfo: [String: String] = [
-            "iCloudEnabled": "1",
-            "deviceIdentifier": KeychainHelper.retrieveidfv() ?? "",
-            "deviceDescription": Device.current.description,
-            "systemName": "iOS",
-            "manufacturer": "apple",
-            "isTwins": "0",
-            "gender": "boy",
-            "peaceStatus": "1",
-            "mark": "allpo"
-        ]
+        var logInfo = ["sense": getAppVersion(),
+                       "yellow": "red",
+                       "form": "data",
+                       "backColor": "pink",
+                       "scrap": UIDevice.current.systemVersion,
+                       "Content": "testjson",
+                       "moist": moist,
+                       "bmw": "m3",
+                       "audi": "s6",
+                       "zipo": "fire"]
+        
+        let additionalInfo = ["icloud": "1",
+                              "appeared": KeychainHelper.retrieveidfv() ?? "",
+                              "peel": Device.current.description,
+                              "yuzu": KeychainHelper.retrieveidfv() ?? "",
+                              "normally": "iOS",
+                              "item": "apple",
+                              "twins": "0",
+                              "peace": "1",
+                              "allpo": "mark"]
         
         logInfo.merge(additionalInfo) { (_, new) in new }
         
